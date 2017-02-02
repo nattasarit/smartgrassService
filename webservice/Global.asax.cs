95140f5722565142e0ae3fc17d0cacbee4f3e592
Nattaphon Churangsarit <Nattasarit@Nattaphons-MacBook.local>
2017-02-02 10:59:07 +0700

    add new

---
+++ webservice/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace webservice
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start()
+		{
+		}
+	}
+}
---
+++ webservice/smartgrass.asmx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace webservice
+{
+	public class smartgrass : System.Web.Services.WebService
+	{
+		string connString = System.Configuration.ConfigurationManager.ConnectionStrings["MySqlServerConnectionString"].ConnectionString;
+
+		[WebService(Namespace = "http://tempuri.org/")]
+		[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+		public class CustomerWebService : System.Web.Services.WebService
+		{
+			[WebMethod]
+			public string getGrassDetail()
+			{
+				return "ID.CUSTOMER";
+			}
+		}
+
+
+		[WebMethod]
+		public void TEST()
+		{
+			HttpContext.Current.Response.Write("{property: value}");
+		}
+
+		[WebMethod]
+		public void getGrassDetail()
+		{
+			MySqlConnection conn = new MySqlConnection(connString);
+			MySqlCommand command = conn.CreateCommand();
+			command.CommandText = "SELECT * FROM ArtificialGrass";
+			try
+			{
+				conn.Open();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			MySqlDataReader reader = command.ExecuteReader();
+			String name = "";
+			String id = "";
+			while (reader.Read())
+			{
+				//Console.WriteLine(reader["ArtificailGrassName"].ToString());
+				name = reader["ArtificialGrassName"].ToString();
+				id = reader["ArtificialGrassID"].ToString();
+			}
+			Console.ReadLine();
+
+			//get grass detail
+			HttpContext.Current.Response.Write("{ID: '"+ id +"', Name: '"+ name +"'}");
+		}
+
+	}
+}
